1fdcdf61f59b6875199f152396f158510add710e
mithun-27 <kvl202014@gmail.com>
2025-10-31 18:45:01 +0530

    leetcode with answers

--- Maximum Frequency of an Element After Performing Operations II.py
+++ Maximum Frequency of an Element After Performing Operations II.py
@@ -1,40 +1,33 @@
-class Solution:
-    
-    def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
-        nums.sort()
-        
-        arrayValMaxFreq = self.maxFrequencyOfArrayVal(nums, k , numOperations) 
+"""You are given an integer array nums and two integers k and numOperations.
 
-        
-        left = 0 
-        otherValMaxFreq = 0
-        for right in range(len(nums)):
-            
-            while nums[right] > nums[left] + 2*k:
-                left += 1
-            
-            otherValMaxFreq = max(otherValMaxFreq, right - left + 1)
-            
-            if otherValMaxFreq >= numOperations:
-                otherValMaxFreq = numOperations
-                break
-            
-        return max(arrayValMaxFreq, otherValMaxFreq)
+You must perform an operation numOperations times on nums, where in each operation you:
 
+Select an index i that was not selected in any previous operations.
+Add an integer in the range [-k, k] to nums[i].
+Return the maximum possible frequency of any element in nums after performing the operations.
 
-    
-    def maxFrequencyOfArrayVal(self, nums, k, numOperations):
-        
-        count = Counter(nums)
+ 
 
-        maxFreq = 0
-        
-        for val in count.keys():
-            left = bisect_left(nums, val - k)
-            right = bisect_right(nums, val + k) - 1
-            freq = min(right - left + 1, numOperations + count[val])
-            maxFreq = max(maxFreq, freq)
+Example 1:
 
-        return maxFreq
+Input: nums = [1,4,5], k = 1, numOperations = 2
 
-        
+Output: 2
+
+Explanation:
+
+We can achieve a maximum frequency of two by:
+
+Adding 0 to nums[1], after which nums becomes [1, 4, 5].
+Adding -1 to nums[2], after which nums becomes [1, 4, 4].
+Example 2:
+
+Input: nums = [5,11,20,20], k = 5, numOperations = 1
+
+Output: 2
+
+Explanation:
+
+We can achieve a maximum frequency of two by:
+
+Adding 0 to nums[1]."""

609c07f107e70690dc04ece8a3e2b06f2ceb251e
mithun-27 <kvl202014@gmail.com>
2025-11-01 21:43:28 +0530

    leetcode with answers

--- Check If Digits Are Equal in String After Operations I.py
+++ Check If Digits Are Equal in String After Operations I.py
@@ -1,19 +1,40 @@
-class Solution:
-    def hasSameDigits(self, s: str) -> bool:
-        n = len(s)
-        from math import comb
-        
-        row_len = n - 2
-        
-        coeff = [comb(row_len, i) % 10 for i in range(row_len + 1)]
-        
-        num1 = 0
-        for i in range(0, n-1):
-            num1 = (num1 + coeff[i] * (ord(s[i]) - ord('0'))) % 10
-        
-        
-        num2 = 0
-        for i in range(1, n):
-            num2 = (num2 + coeff[i-1] * (ord(s[i]) - ord('0'))) % 10
-        
-        return num1 == num2
+"""You are given a string s consisting of digits. Perform the following operation repeatedly until the string has exactly two digits:
+
+For each pair of consecutive digits in s, starting from the first digit, calculate a new digit as the sum of the two digits modulo 10.
+Replace s with the sequence of newly calculated digits, maintaining the order in which they are computed.
+Return true if the final two digits in s are the same; otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: s = "3902"
+
+Output: true
+
+Explanation:
+
+Initially, s = "3902"
+First operation:
+(s[0] + s[1]) % 10 = (3 + 9) % 10 = 2
+(s[1] + s[2]) % 10 = (9 + 0) % 10 = 9
+(s[2] + s[3]) % 10 = (0 + 2) % 10 = 2
+s becomes "292"
+Second operation:
+(s[0] + s[1]) % 10 = (2 + 9) % 10 = 1
+(s[1] + s[2]) % 10 = (9 + 2) % 10 = 1
+s becomes "11"
+Since the digits in "11" are the same, the output is true.
+Example 2:
+
+Input: s = "34789"
+
+Output: false
+
+Explanation:
+
+Initially, s = "34789".
+After the first operation, s = "7157".
+After the second operation, s = "862".
+After the third operation, s = "48".
+Since '4' != '8', the output is false."""

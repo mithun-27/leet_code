0a99ab8df0cefbde6a782713a69eacc6870b0f64
mithun-27 <kvl202014@gmail.com>
2025-11-22 19:27:08 +0530

    LeetCode with answers

--- Ones and Zeroes.py
+++ Ones and Zeroes.py
@@ -49,3 +49,11 @@ print(solution.findMaxForm(["10","0001","111001","1","0"], 5, 3))  # Output: 4
 print(solution.findMaxForm(["10","0","1"], 1, 1))  # Output: 2
 print(solution.findMaxForm(["10","0001","111001","1","0"], 3, 3))  # output: 3
 
+"""walkthrough
+1. We define a class Solution with a method findMaxForm that takes a list of binary strings strs and two integers m and n.
+2. We initialize a 2D list dp with dimensions (m+1) x (n+1) filled with zeros. This will be used for dynamic programming to store the maximum subset sizes.
+3. We iterate through each string s in strs.
+4. For each string, we count the number of '0's (z) and '1's (o).
+5. We then update the dp table in reverse order to avoid overwriting values that we still need to use.
+6. Finally, we return the value at dp[m][n], which contains the size of the largest subset that can be formed with at most m '0's and n '1's.
+"""

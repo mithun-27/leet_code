c06e98ca7bda43741522a92d855721bf54762c2d
mithun-27 <kvl202014@gmail.com>
2025-11-29 23:04:37 +0530

    LeetCode with answers

---
+++ Check If All 1's Are at Least Length K Places Away.py
@@ -0,0 +1,36 @@
+"""Given an binary array nums and an integer k, return true if all 1's are at least k places away from each other, otherwise return false.
+
+ 
+
+Example 1:
+
+
+Input: nums = [1,0,0,0,1,0,0,1], k = 2
+Output: true
+Explanation: Each of the 1s are at least 2 places away from each other.
+Example 2:
+
+
+Input: nums = [1,0,0,1,0,1], k = 2
+Output: false
+Explanation: The second 1 and third 1 are only one apart from each other.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+0 <= k <= nums.length
+nums[i] is 0 or 1"""
+
+#answer
+from typing import List
+
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        prev = -float('inf')  # index of last seen 1; start far left
+        for i, v in enumerate(nums):
+            if v == 1:
+                if i - prev - 1 < k:
+                    return False
+                prev = i
+        return True

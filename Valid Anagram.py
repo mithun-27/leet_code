eb33ad5bdfe84a94b921e329ca7e2206f7af4eec
mithun-27 <kvl202014@gmail.com>
2025-12-12 19:36:01 +0530

    leet code with answers

--- Valid Anagram.py
+++ Valid Anagram.py
@@ -20,3 +20,18 @@ Constraints:
 
 1 <= s.length, t.length <= 5 * 104
 s and t consist of lowercase English letters."""
+
+
+#answer
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT

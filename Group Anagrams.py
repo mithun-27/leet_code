00a671895a116a8eb39e9865abe50b1fae7adef7
mithun-27 <kvl202014@gmail.com>
2025-12-14 20:00:49 +0530

    leet code with answers

--- Group Anagrams.py
+++ Group Anagrams.py
@@ -32,3 +32,15 @@ Constraints:
 1 <= strs.length <= 104
 0 <= strs[i].length <= 100
 strs[i] consists of lowercase English letters."""
+
+#answer
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
+

b395f7536ee798fb229c883efe7398286963697e
mithun-27 <kvl202014@gmail.com>
2025-12-30 23:13:15 +0530

    Leetcode with answers

--- Find Minimum in Rotated Sorted Array.py
+++ Find Minimum in Rotated Sorted Array.py
@@ -34,3 +34,15 @@ n == nums.length
 -5000 <= nums[i] <= 5000
 All the integers of nums are unique.
 nums is sorted and rotated between 1 and n times."""
+
+#answer
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        while l < r:
+            m = l + (r - l) // 2
+            if nums[m] < nums[r]:
+                r = m
+            else:
+                l = m + 1
+        return nums[l]

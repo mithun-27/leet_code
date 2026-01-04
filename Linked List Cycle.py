a373acbd767301422a46d3f0c8e21a316b339bda
mithun-27 <kvl202014@gmail.com>
2026-01-04 18:56:51 +0530

    Leetcode with answers

--- Linked List Cycle.py
+++ Linked List Cycle.py
@@ -32,3 +32,22 @@ The number of the nodes in the list is in the range [0, 104].
 -105 <= Node.val <= 105
 pos is -1 or a valid index in the linked-list.
  """
+
+#answer
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False

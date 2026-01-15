0af4d69f58d190dd203adacf80fda2297e059586
mithun-27 <kvl202014@gmail.com>
2026-01-15 19:38:29 +0530

    Leetcode with answers

--- Validate Binary Search Tree.py
+++ Validate Binary Search Tree.py
@@ -1,24 +1,26 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+"""Given the root of a binary tree, determine if it is a valid binary search tree (BST).
 
-class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
+A valid BST is defined as follows:
 
-        q = deque([(root, float("-inf"), float("inf"))])
+The left subtree of a node contains only nodes with keys strictly less than the node's key.
+The right subtree of a node contains only nodes with keys strictly greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
 
-        while q:
-            node, left, right = q.popleft()
-            if not (left < node.val < right):
-                return False
-            if node.left:
-                q.append((node.left, left, node.val))
-            if node.right:
-                q.append((node.right, node.val, right))
+Example 1:
 
-        return True
+
+Input: root = [2,1,3]
+Output: true
+Example 2:
+
+
+Input: root = [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-231 <= Node.val <= 231 - 1"""

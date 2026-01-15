2468798f7a15dab99d5b26ecab2bcad86dd930e7
mithun-27 <kvl202014@gmail.com>
2026-01-15 19:38:54 +0530

    Leetcode with answers

--- Validate Binary Search Tree.py
+++ Validate Binary Search Tree.py
@@ -24,3 +24,29 @@ Constraints:
 
 The number of nodes in the tree is in the range [1, 104].
 -231 <= Node.val <= 231 - 1"""
+
+#answer
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        q = deque([(root, float("-inf"), float("inf"))])
+
+        while q:
+            node, left, right = q.popleft()
+            if not (left < node.val < right):
+                return False
+            if node.left:
+                q.append((node.left, left, node.val))
+            if node.right:
+                q.append((node.right, node.val, right))
+
+        return True

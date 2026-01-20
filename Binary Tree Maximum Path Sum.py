dc7dfe3234f6f7dcdc4970889926a21cddfe7498
mithun-27 <kvl202014@gmail.com>
2026-01-20 15:46:30 +0530

    Leetcode with answers

--- Binary Tree Maximum Path Sum.py
+++ Binary Tree Maximum Path Sum.py
@@ -24,3 +24,30 @@ Constraints:
 
 The number of nodes in the tree is in the range [1, 3 * 104].
 -1000 <= Node.val <= 1000"""
+
+#answer
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = [root.val]
+
+        def dfs(root):
+            if not root:
+                return 0
+
+            leftMax = dfs(root.left)
+            rightMax = dfs(root.right)
+            leftMax = max(leftMax, 0)
+            rightMax = max(rightMax, 0)
+
+            res[0] = max(res[0], root.val + leftMax + rightMax)
+            return root.val + max(leftMax, rightMax)
+
+        dfs(root)
+        return res[0]

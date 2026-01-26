7288634fd67cbc66f30f6d48bee337ec91f96634
mithun-27 <kvl202014@gmail.com>
2026-01-26 23:08:33 +0530

    Leetcode with answers

--- Implement Trie (Prefix Tree).py
+++ Implement Trie (Prefix Tree).py
@@ -31,3 +31,39 @@ Constraints:
 1 <= word.length, prefix.length <= 2000
 word and prefix consist only of lowercase English letters.
 At most 3 * 104 calls in total will be made to insert, search, and startsWith."""
+
+class Trie:
+
+    def __init__(self):
+        self.root={}
+        
+    def insert(self, word: str) -> None:
+
+        cur=self.root
+
+        for letter in word:
+            if letter not in cur:
+                cur[letter]={}
+            cur=cur[letter]
+
+        cur['*']=''
+
+    def search(self, word: str) -> bool:
+
+        cur=self.root
+        for letter in word:
+            if letter not in cur:
+                return False
+            cur=cur[letter]
+
+        return '*' in cur
+        
+    def startsWith(self, prefix: str) -> bool:
+
+        cur=self.root
+        for letter in prefix:
+            if letter not in cur:
+                return False
+            cur=cur[letter]
+
+        return True

b1d0b0dd1ba0aba6cfb4517bc3149837a687a02b
mithun-27 <kvl202014@gmail.com>
2026-01-29 21:08:14 +0530

    Leetcode with answers

--- Clone Graph.py
+++ Clone Graph.py
@@ -50,3 +50,31 @@ The number of nodes in the graph is in the range [0, 100].
 Node.val is unique for each node.
 There are no repeated edges and no self-loops in the graph.
 The Graph is connected and all nodes can be visited starting from the given node."""
+
+#answer
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+
+        oldToNew = {}
+        oldToNew[node] = Node(node.val)
+        q = deque([node])
+
+        while q:
+            cur = q.popleft()
+            for nei in cur.neighbors:
+                if nei not in oldToNew:
+                    oldToNew[nei] = Node(nei.val)
+                    q.append(nei)
+                oldToNew[cur].neighbors.append(oldToNew[nei])
+
+        return oldToNew[node]

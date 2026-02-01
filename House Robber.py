943e6e6e1d93b50f4c3b67a565b9833243fd614f
mithun-27 <kvl202014@gmail.com>
2026-02-01 20:07:52 +0530

    Leetcode with answers

--- House Robber.py
+++ House Robber.py
@@ -24,6 +24,9 @@ Constraints:
 0 <= nums[i] <= 400"""
 
 #answer
+from git import List
+
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
         rob1, rob2 = 0, 0
@@ -33,3 +36,8 @@ class Solution:
             rob1 = rob2
             rob2 = temp
         return rob2
+    
+#example usage
+Solution().rob([1,2,3,1])  # Example call to the function
+Solution().rob([2,7,9,3,1])  # Example call to the function
+print(Solution().rob([2,1,1,2]))  # Example call to the function         4

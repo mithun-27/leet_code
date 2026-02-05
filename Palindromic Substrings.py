f5f615c5b2d4af22fff0a4d16aa3b49705e65a25
mithun-27 <kvl202014@gmail.com>
2026-02-05 22:58:54 +0530

    Leetcode with answers

--- Palindromic Substrings.py
+++ Palindromic Substrings.py
@@ -1,22 +1,24 @@
-class Solution:
-    def countSubstrings(self, s: str) -> int:
+"""Given a string s, return the number of palindromic substrings in it.
 
-        def manacher(s):
-            t = '#' + '#'.join(s) + '#'
-            n = len(t)
-            p = [0] * n
-            l, r = 0, 0
-            for i in range(n):
-                p[i] = min(r - i, p[l + (r - i)]) if i < r else 0
-                while (i + p[i] + 1 < n and i - p[i] - 1 >= 0
-                       and t[i + p[i] + 1] == t[i - p[i] - 1]):
-                    p[i] += 1
-                if i + p[i] > r:
-                    l, r = i - p[i], i + p[i]
-            return p
+A string is a palindrome when it reads the same backward as forward.
 
-        p = manacher(s)
-        res = 0
-        for i in p:
-            res += (i + 1) // 2
-        return res
+A substring is a contiguous sequence of characters within the string.
+
+ 
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of lowercase English letters."""

ab0a51876e55d00944c42128ce8d8b810468c18c
mithun-27 <kvl202014@gmail.com>
2026-02-11 21:32:34 +0530

    Leetcode with answers

--- Longest Increasing Subsequence.py
+++ Longest Increasing Subsequence.py
@@ -24,3 +24,22 @@ Constraints:
  
 
 Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?"""
+
+#answer
+from bisect import bisect_left
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = []
+        dp.append(nums[0])
+
+        LIS = 1
+        for i in range(1, len(nums)):
+            if dp[-1] < nums[i]:
+                dp.append(nums[i])
+                LIS += 1
+                continue
+
+            idx = bisect_left(dp, nums[i])
+            dp[idx] = nums[i]
+
+        return LIS

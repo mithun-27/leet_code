6c41b1efd7fabe39616f2b5c46204808989c5e98
mithun-27 <kvl202014@gmail.com>
2026-02-12 21:25:41 +0530

    Leetcode with answers

--- Unique Paths.py
+++ Unique Paths.py
@@ -24,3 +24,19 @@ Explanation: From the top-left corner, there are a total of 3 ways to reach the 
 Constraints:
 
 1 <= m, n <= 100"""
+
+#answer
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 1 or n == 1:
+            return 1
+        if m < n:
+            m, n = n, m
+
+        res = j = 1
+        for i in range(m, m + n - 1):
+            res *= i
+            res //= j
+            j += 1
+
+        return res
